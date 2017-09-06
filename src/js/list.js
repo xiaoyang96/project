01ceccd33b0c4c421601feb9ac1a2c0e37093d24
xiaoyang96 <961389562@qq.com>
2017-09-06 22:45:26 +0800

    列表页

--- src/js/list.js
+++ src/js/list.js
@@ -2,12 +2,15 @@
 * @Author: Marte
 * @Date:   2017-09-02 16:10:56
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-09-02 16:36:17
+* @Last Modified time: 2017-09-06 12:01:09
 */
 
 require(['config'],function(){
     require(['jquery'],function($){
         $('.footer').load('reg.html .footer');
 
+
+        
     });
 })
+
